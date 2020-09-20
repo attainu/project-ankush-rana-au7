7c931e5da72226e23f6afb425b4263c105e7c5e6
ANKUSH RANA <ankushrana830@gmail.com>
2020-09-20 22:40:26 +0530

    models for user,category,product,order created and database connection setup done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DATABASE IS CONNECTED");
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`SERVER STARTED AT PORT:${port}`);
+  console.log(`SERVER IS RUNNING ON PORT:${port}`);
 });
---
+++ models/category.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = moongoose.model("Category", categorySchema);
